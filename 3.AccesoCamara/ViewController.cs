2388ce772c036040d2a76a87837eac134db87908
Alejandro Ariza <diego.ariza@accenture.com>
2019-09-03 20:53:14 -0500

    🎇 Se agregan Validaciones de Cámara
    Se agregan validaciones para identificar si la App se esta ejecutando desde un dispositivo físico.
    
     On branch develop
     Your branch is up to date with 'origin/develop'.
    
     Changes to be committed:
    	modified:   3.AccesoCamara/Info.plist
    	modified:   3.AccesoCamara/ViewController.cs

--- 3.AccesoCamara/ViewController.cs
+++ 3.AccesoCamara/ViewController.cs
@@ -1,6 +1,8 @@
 ﻿using Foundation;
 using System;
 using UIKit;
+using System.IO;
+using Foundation;
 
 namespace _3.AccesoCamara
 {
@@ -9,8 +11,6 @@ namespace _3.AccesoCamara
         //Decalración de variables.
         UIImagePickerController SeleccionImagen;
         UIImage Fotografia; //Objecto de tipo image.
-
-
 
         public ViewController(IntPtr handle) : base(handle)
         {
@@ -27,10 +27,22 @@ namespace _3.AccesoCamara
             SeleccionImagen.Canceled += CanceledaSeleccion; //Evento de cancelación.
 
 
+            if (UIImagePickerController.IsSourceTypeAvailable(UIImagePickerControllerSourceType.Camera))// Validar si el dispositivo tiene camara.
+            {
+
+                SeleccionImagen.SourceType = UIImagePickerControllerSourceType.Camera;
+
+            }
+            else //no tiene camara.
+            {
+
+                SeleccionImagen.SourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+
+            }
+
             btnCamara.TouchUpInside += delegate {
 
-                SeleccionImagen.SourceType = UIImagePickerControllerSourceType.Camera; // Se selecciona el origen "Biblioteca"
-
+             
                 PresentViewController(SeleccionImagen,
                                         true,
                                         null);
@@ -40,11 +52,13 @@ namespace _3.AccesoCamara
 
             btnBiblioteca.TouchUpInside += delegate {
 
-                SeleccionImagen.SourceType = UIImagePickerControllerSourceType.PhotoLibrary; // Se selecciona el origen "Camara"
+                string rutaCarpeta = Environment.GetFolderPath(Environment.SpecialFolder.Personal);//Ruta del dispositivo
 
-                PresentViewController(SeleccionImagen,
-                                        true,
-                                        null);
+                string nombreArchivo = "FotoN0rf3n01.jpg"; //nombre del archivo
+
+                string rutaCompleta = Path.Combine(rutaCarpeta, nombreArchivo);
+
+                imagenes.Image = UIImage.FromFile(rutaCompleta);
 
             };
 
@@ -66,7 +80,34 @@ namespace _3.AccesoCamara
 
             imagenes.Image = Fotografia; //Se envia al componente grafico.
 
-            SeleccionImagen.DismissViewControllerAsync(true); //Quitar la pantalla del picker Controller
+            Fotografia.SaveToPhotosAlbum(delegate (UIImage imagen, NSError error)
+            {
+
+                if(null != error) //Si, el error contiene información, debe mostrarla.
+                {
+                    Console.WriteLine(error.LocalizedDescription);//muestra la información del error
+                }
+            }
+            );
+
+            Fotografia.AsJPEG(); //Almacena en el album de photografias.
+
+            string rutaCarpeta = Environment.GetFolderPath(Environment.SpecialFolder.Personal);//Ruta del dispositivo
+
+            string nombreArchivo = "Foto01.jpg"; //nombre del archivo
+
+            string rutaCompleta = Path.Combine(rutaCarpeta, nombreArchivo);
+
+            NSError err = null;
+
+            NSData img  = Fotografia.AsJPEG(); //Almacena en el album de photografias, Se selecciona la imagen que esta guardad localmente.
+
+            img.Save(rutaCompleta, false, out err);
+
+            imagenes.Image = UIImage.FromFile(rutaCompleta);
+
+            SeleccionImagen.DismissViewControllerAsync(true);
+
 
         }
 
