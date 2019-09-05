16356d7de336429fead0d5627a647aaafbf93cef
Alejandro Ariza <diego.ariza@accenture.com>
2019-09-05 08:40:04 -0500

    ☠️  Se agrega lógica para reconocer los gestos.
    Se agrega la lógica para reconocer los gesto y se esta generando una excepcion con el objecto UIGestureRecognizer
     On branch develop
     Your branch is up to date with 'origin/develop'.
    
     Changes to be committed:
    	modified:   4.Gestos/4.Gestos.csproj
    	modified:   4.Gestos/LaunchScreen.storyboard
    	modified:   4.Gestos/Main.storyboard
    	modified:   4.Gestos/ViewController.cs
    	modified:   4.Gestos/ViewController.designer.cs
    	new file:   4.Gestos/daemon.png

--- 4.Gestos/ViewController.cs
+++ 4.Gestos/ViewController.cs
@@ -1,11 +1,25 @@
 ﻿using Foundation;
 using System;
 using UIKit;
+using CoreGraphics;
 
 namespace _4.Gestos
 {
     public partial class ViewController : UIViewController
     {
+
+        nfloat rotacion = 0;
+        nfloat coordenadasX = 0;
+        nfloat coordenadasY = 0;
+
+        bool Toque;
+
+        UIRotationGestureRecognizer GestoRotar;
+        UIPanGestureRecognizer GestoMover;
+        UIAlertController ALerta;
+
+
+
         public ViewController(IntPtr handle) : base(handle)
         {
         }
@@ -13,13 +27,101 @@ namespace _4.Gestos
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+
+            imagen.UserInteractionEnabled = true; //
+            var GestoToque = new UITapGestureRecognizer(Tocando);
+
+            //Se aplican los gestos a la imagen
+            imagen.AddGestureRecognizer(GestoToque);
+
+            GestoMover = (UIKit.UIPanGestureRecognizer)new UIGestureRecognizer(() =>
+            {
+
+                if ((GestoMover.State == UIGestureRecognizerState.Began || //Validar el estado del gesto
+                    GestoMover.State == UIGestureRecognizerState.Changed) &&
+                    (GestoMover.NumberOfTouches == 1))
+                {
+
+                    var P0 = GestoMover.LocationInView(View); //Gesto en toda la pantalla.
+
+                    if (coordenadasX == 0)
+                        coordenadasX = P0.X - imagen.Center.X;
+
+                    if (coordenadasY == 0)
+                        coordenadasY = P0.Y - imagen.Center.Y;
+
+                    var P1 = new CGPoint(P0.X - coordenadasX, P0.Y - coordenadasY);
+
+                    imagen.Center = P1;
+
+                }
+                else
+                {
+
+                    coordenadasX = 0;
+                    coordenadasY = 0;
+
+                }
+            });
+
+
+            GestoRotar = (UIKit.UIRotationGestureRecognizer)new UIGestureRecognizer(() =>
+            {
+
+                if ((GestoRotar.State == UIGestureRecognizerState.Began || //Validar el estado del gesto
+                    GestoRotar.State == UIGestureRecognizerState.Changed) &&
+                    (GestoRotar.NumberOfTouches == 2)) //el gesto de rotar se hace con dos toques.
+                {
+                    imagen.Transform = CGAffineTransform.MakeRotation(GestoRotar.Rotation + rotacion); //Se vuelve a verificar el estado, para validar en donde se quedo.
+                }
+                else if (GestoRotar.State == UIGestureRecognizerState.Ended)
+                {
+                    rotacion = GestoRotar.Rotation; //Se obtinen los valores de rotación.
+                }
+            });
+
+            //Se aplican los gestos a la imagen
+            imagen.AddGestureRecognizer(GestoMover);
+            imagen.AddGestureRecognizer(GestoRotar);
+
         }
 
-        public override void DidReceiveMemoryWarning()
+
+        void Tocando(UITapGestureRecognizer toque)
         {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
+            if (!Toque)
+            {
+                toque.View.Transform *= CGAffineTransform.MakeRotation((float)Math.PI);
+                Toque = true;
+                ALerta = UIAlertController.Create("Imagen Tocada",//Titulo
+                                                  "Imagen Girando",//Mensaje
+                                                  UIAlertControllerStyle.Alert);
+
+                ALerta.AddAction(UIAlertAction.Create("Aceptar", //Se agrega un botón al alert.
+                                                      UIAlertActionStyle.Default, //estilo del sistema operativo
+                                                      null));
+
+                PresentViewController(ALerta, true, null); //Mostrar el alert al usuario.
+
+            }
+            else
+            {
+
+                toque.View.Transform *= CGAffineTransform.MakeRotation((float)-Math.PI);
+                Toque = false;
+                ALerta = UIAlertController.Create("Imagen Regresa",//Titulo
+                                                  "Imagen Regresando",//Mensaje
+                                                  UIAlertControllerStyle.Alert);
+
+                ALerta.AddAction(UIAlertAction.Create("Aceptar", //Se agrega un botón al alert.
+                                                      UIAlertActionStyle.Default, //estilo del sistema operativo
+                                                      null));
+
+                PresentViewController(ALerta, true, null); //Mostrar el alert al usuario.
+
+            }
         }
+
+ 
     }
 }
