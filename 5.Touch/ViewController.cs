c37fe1ce8525502cdc09a6d42e5e14d5206d9e4c
Alejandro Ariza <diego.ariza@accenture.com>
2019-09-05 22:07:49 -0500

    Lógica invocación touch ID  On branch develop  Your branch is up to date with 'origin/develop'.
     Changes to be committed:
    	modified:   5.Touch/Info.plist
    	modified:   5.Touch/LaunchScreen.storyboard
    	modified:   5.Touch/Main.storyboard
    	modified:   5.Touch/ViewController.cs
    	modified:   5.Touch/ViewController.designer.cs

--- 5.Touch/ViewController.cs
+++ 5.Touch/ViewController.cs
@@ -1,25 +1,79 @@
 ﻿using Foundation;
 using System;
 using UIKit;
+using CoreAnimation;
+using CoreGraphics;
+using CoreMotion;
+using LocalAuthentication;
+using System.Threading;
 
 namespace _5.Touch
 {
     public partial class ViewController : UIViewController
     {
+        CMMotionActivityManager Movimiento; //Utilizar el Acelerometro
+        nfloat Altura;
+        nfloat Ancho;
+        nfloat AnchoCapaa;
+        nfloat AlturaCapa;
+        nfloat ActualizaX;
+        nfloat ActualizaY;
+        CALayer CapaImagen3;//Capa
+
         public ViewController(IntPtr handle) : base(handle)
         {
         }
 
-        public override void ViewDidLoad()
+        public override async void ViewDidLoad()
         {
+
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+
+            Console.WriteLine("N0rf3n - ViewDidLoad - Begin");
+
+            try
+            {
+
+                Console.WriteLine("N0rf3n - ViewDidLoad - Begin/Try");
+
+                var Verifica = new LAContext(); //Context
+                var myReason = new NSString("Autenticación Biométrica");
+                var autoriza = await Verifica.EvaluatePolicyAsync(LAPolicy.DeviceOwnerAuthenticationWithBiometrics,//Await, es un proceso Asyncrono y toca cambiar el  public override void  por ->  public override async void y vamos a usar la validación biometrica.
+                    myReason);
+
+                if (autoriza.Item1) //el primer elemento Item1 hace refenrecia al OK y el segundo al error.
+                {
+                    Console.WriteLine("N0rf3n - ViewDidLoad - Ingreso App - TouchID");
+                }
+                else
+                {
+                   // System.Threading.Thread.CurrentThread.Abort();//Salga de la venta,  la coloque en segundo plano y coloque la App en primer plano
+                    Console.WriteLine("N0rf3n - ViewDidLoad - Else");
+                    Thread.CurrentThread.Abort();
+                }
+
+                Console.WriteLine("N0rf3n - ViewDidLoad - End");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("N0rf3n - ViewDidLoad - End/Catch Error : " + ex.Message);
+
+                var alerta = UIAlertController.Create("Estado",
+                                              ex.Message,
+                                              UIAlertControllerStyle.Alert);
+
+
+                alerta.AddAction(UIAlertAction.Create("Aceptar", UIAlertActionStyle.Default, null));
+
+                PresentViewController(alerta, true, null);
+
+
+            }
+            
         }
 
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
-        }
+
     }
 }
--- 5.Touch/ViewController.designer.cs
+++ 5.Touch/ViewController.designer.cs
@@ -1,18 +1,42 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace _5.Touch
 {
-    [Register("ViewController")]
-    partial class ViewController
-    {
-    }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UILabel lblX { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblY { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblZ { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (lblX != null) {
+				lblX.Dispose ();
+				lblX = null;
+			}
+
+			if (lblY != null) {
+				lblY.Dispose ();
+				lblY = null;
+			}
+
+			if (lblZ != null) {
+				lblZ.Dispose ();
+				lblZ = null;
+			}
+		}
+	}
 }
