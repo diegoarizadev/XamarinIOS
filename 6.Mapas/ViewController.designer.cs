c3987d28dc7a94b396ceef283c35ae3be55f2516
Alejandro Ariza <diego.ariza@accenture.com>
2019-09-08 00:59:18 -0500

    📱Creación y actualización Main.StoryBoard.
     On branch develop
     Your branch is up to date with 'origin/develop'.
    
     Changes to be committed:
    	modified:   6.Mapas/LaunchScreen.storyboard
    	modified:   6.Mapas/Main.storyboard
    	modified:   6.Mapas/ViewController.designer.cs

--- 6.Mapas/ViewController.designer.cs
+++ 6.Mapas/ViewController.designer.cs
@@ -1,18 +1,66 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace _6.Mapas
 {
-    [Register("ViewController")]
-    partial class ViewController
-    {
-    }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UILabel lblCiudad { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblDepartamento { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblLatitud { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblLongitud { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblMunicipio { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblPais { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (lblPais != null) {
+				lblPais.Dispose ();
+				lblPais = null;
+			}
+
+			if (lblDepartamento != null) {
+				lblDepartamento.Dispose ();
+				lblDepartamento = null;
+			}
+
+			if (lblCiudad != null) {
+				lblCiudad.Dispose ();
+				lblCiudad = null;
+			}
+
+			if (lblMunicipio != null) {
+				lblMunicipio.Dispose ();
+				lblMunicipio = null;
+			}
+
+			if (lblLatitud != null) {
+				lblLatitud.Dispose ();
+				lblLatitud = null;
+			}
+
+			if (lblLongitud != null) {
+				lblLongitud.Dispose ();
+				lblLongitud = null;
+			}
+		}
+	}
 }
