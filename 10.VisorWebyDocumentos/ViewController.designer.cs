fe85940c1c7a00b65713b045f598062cb5fcc40c
Alejandro Ariza <diego.ariza@accenture.com>
2019-09-10 22:10:16 -0500

    ⭕️ Se agregan Objectos al StoryBoard, Constrains, Outlets y Documentos de Office.
    On branch develop
    Your branch is up to date with 'origin/develop'.
    
     Changes to be committed:
    	modified:   10.VisorWebyDocumentos/10.VisorWebyDocumentos.csproj
    	new file:   10.VisorWebyDocumentos/Docs/DocXamarin.docx
    	new file:   10.VisorWebyDocumentos/Docs/LibroXamarin.xlsx
    	new file:   10.VisorWebyDocumentos/Docs/PresentaciónXamarin.pptx
    	modified:   10.VisorWebyDocumentos/LaunchScreen.storyboard
    	modified:   10.VisorWebyDocumentos/Main.storyboard
    	modified:   10.VisorWebyDocumentos/ViewController.designer.cs

--- 10.VisorWebyDocumentos/ViewController.designer.cs
+++ 10.VisorWebyDocumentos/ViewController.designer.cs
@@ -1,18 +1,50 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace _10.VisorWebyDocumentos
 {
-    [Register("ViewController")]
-    partial class ViewController
-    {
-    }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UIButton btnDocumentos { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnVisorWeb1 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnVisorWeb2 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnVisorWeb3 { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (btnDocumentos != null) {
+				btnDocumentos.Dispose ();
+				btnDocumentos = null;
+			}
+
+			if (btnVisorWeb1 != null) {
+				btnVisorWeb1.Dispose ();
+				btnVisorWeb1 = null;
+			}
+
+			if (btnVisorWeb2 != null) {
+				btnVisorWeb2.Dispose ();
+				btnVisorWeb2 = null;
+			}
+
+			if (btnVisorWeb3 != null) {
+				btnVisorWeb3.Dispose ();
+				btnVisorWeb3 = null;
+			}
+		}
+	}
 }
