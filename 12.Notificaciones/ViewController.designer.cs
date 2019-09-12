54e28bbbf29432241849a643b371983ad1e078f7
Alejandro Ariza <diego.ariza@accenture.com>
2019-09-12 07:56:14 -0500

    📧 Se agrega ViewController en el StoryBoard y sus constrains
     On branch develop
     Your branch is up to date with 'origin/develop'.
    
    Changes to be committed:
    	modified:   12.Notificaciones/12.Notificaciones.csproj
    	modified:   12.Notificaciones/Main.storyboard
    	new file:   12.Notificaciones/Notificacion.gif
    	modified:   12.Notificaciones/ViewController.designer.cs

--- 12.Notificaciones/ViewController.designer.cs
+++ 12.Notificaciones/ViewController.designer.cs
@@ -1,18 +1,58 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace _12.Notificaciones
 {
-    [Register("ViewController")]
-    partial class ViewController
-    {
-    }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UIButton btnNotificacionesCalendario { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnNotificacionesIntervalo { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnNotificacionesRespuesta { get; set; }
+
+		[Outlet]
+		UIKit.UIDatePicker Calendario { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblRespuesta { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (btnNotificacionesIntervalo != null) {
+				btnNotificacionesIntervalo.Dispose ();
+				btnNotificacionesIntervalo = null;
+			}
+
+			if (btnNotificacionesCalendario != null) {
+				btnNotificacionesCalendario.Dispose ();
+				btnNotificacionesCalendario = null;
+			}
+
+			if (btnNotificacionesRespuesta != null) {
+				btnNotificacionesRespuesta.Dispose ();
+				btnNotificacionesRespuesta = null;
+			}
+
+			if (lblRespuesta != null) {
+				lblRespuesta.Dispose ();
+				lblRespuesta = null;
+			}
+
+			if (Calendario != null) {
+				Calendario.Dispose ();
+				Calendario = null;
+			}
+		}
+	}
 }
