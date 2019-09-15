817790dfc7c7d7aa9c3099335fd6c1e91ec62d2f
Alejandro Ariza <diego.ariza2@gmail.com>
2019-09-15 01:16:20 -0500

    🎩 Se agrega elementos gráficos y constrains al Storyboard del proyecto así como iconos de la App.

--- 14.Rest/14.Rest/ViewController.designer.cs
+++ 14.Rest/14.Rest/ViewController.designer.cs
@@ -1,18 +1,58 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace _14.Rest
 {
-    [Register("ViewController")]
-    partial class ViewController
-    {
-    }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UIButton btnConsultarClima { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblCondiciones { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblHumedad { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblLocalidad { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblTemperatura { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (lblLocalidad != null) {
+				lblLocalidad.Dispose ();
+				lblLocalidad = null;
+			}
+
+			if (lblTemperatura != null) {
+				lblTemperatura.Dispose ();
+				lblTemperatura = null;
+			}
+
+			if (lblHumedad != null) {
+				lblHumedad.Dispose ();
+				lblHumedad = null;
+			}
+
+			if (lblCondiciones != null) {
+				lblCondiciones.Dispose ();
+				lblCondiciones = null;
+			}
+
+			if (btnConsultarClima != null) {
+				btnConsultarClima.Dispose ();
+				btnConsultarClima = null;
+			}
+		}
+	}
 }
