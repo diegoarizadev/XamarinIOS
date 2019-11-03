d184b8e3759986fd46aa18bbec1c0901e490dd8d
Alejandro Ariza <diego.ariza2@gmail.com>
2019-11-03 08:15:56 -0500

    🏗 Se agrega XIB para el menu y su controlador
     On branch develop
     Your branch is up to date with 'origin/develop'.
    
     Changes to be committed:
    	modified:   18.Slidingmenu/18.Slidingmenu.csproj
    	new file:   18.Slidingmenu/ViewControllers/MenuTableViewController.cs
    	new file:   18.Slidingmenu/ViewControllers/MenuTableViewController.designer.cs
    	new file:   18.Slidingmenu/ViewControllers/MenuTableViewController.xib

---
+++ 18.Slidingmenu/ViewControllers/MenuTableViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace _18.Slidingmenu.ViewControllers
+{
+    public partial class MenuTableViewController : UITableViewCell
+    {
+        public static readonly UINib Nib = UINib.FromName("MenuTableViewCell", NSBundle.MainBundle);
+
+        public static readonly NSString Key = new NSString("MenuTableViewCell");
+
+        public MenuTableViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public static MenuTableViewController Create()
+        {
+            return (MenuTableViewController)Nib.Instantiate(null, null)[0];
+        }
+
+        internal void BindData(string strLabel, string strIconPath)
+        {
+            lblTexto.Text = strLabel;
+            image.Image = UIImage.FromBundle(strIconPath);
+        }
+    }
+}
+
