3af0ab1eafb4d9fc32f8b40d939aab4cca53cb1a
Alejandro Ariza <diego.ariza2@gmail.com>
2019-11-03 08:15:56 -0500

    🏗 Se agrega XIB para el menu y su controlador
     On branch develop
     Your branch is up to date with 'origin/develop'.
    
     Changes to be committed:
    	modified:   18.Slidingmenu/18.Slidingmenu.csproj
    	new file:   18.Slidingmenu/ViewControllers/MenuTableViewController.cs
    	new file:   18.Slidingmenu/ViewControllers/MenuTableViewController.designer.cs
    	new file:   18.Slidingmenu/ViewControllers/MenuTableViewController.xib

---
+++ 18.Slidingmenu/ViewControllers/MenuTableViewController.designer.cs
@@ -0,0 +1,38 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace _18.Slidingmenu.ViewControllers
+{
+    [Register ("MenuTableViewController")]
+    partial class MenuTableViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView image { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblTexto { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (image != null) {
+                image.Dispose ();
+                image = null;
+            }
+
+            if (lblTexto != null) {
+                lblTexto.Dispose ();
+                lblTexto = null;
+            }
+        }
+    }
+}
